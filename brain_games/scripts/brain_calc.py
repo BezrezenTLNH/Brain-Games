c20b1cc1f7a0e43a6e2da1f97404b1f9c873ebc7
root <root@DESKTOP-7S7HNTG>
2023-03-21 11:15:19 +0300

    first part of stage 6

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import prompt
+from random import randint
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print('What is the result of the expression?')
+    correct_answers = 0
+    while correct_answers < 3:
+        operation_number = randint(1, 3)
+        first_current_random_number = randint(1, 25)
+        second_current_number = randint(1, 25)
+        summ = first_current_random_number + second_current_number
+        multiplication = first_current_random_number * second_current_number
+        subtraction = first_current_random_number - second_current_number
+
+        if operation_number == 1:
+            right_answer = summ
+            print(f'Question: {first_current_random_number} '
+                  f'+ {second_current_number}')
+        elif operation_number == 2:
+            right_answer = multiplication
+            print(f'Question: {first_current_random_number} '
+                  f'* {second_current_number}')
+        else:
+            right_answer = subtraction
+            print(f'Question: {first_current_random_number} '
+                  f'- {second_current_number}')
+
+        user_answer = prompt.integer('Your answer: ')
+
+        if user_answer == right_answer:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(."
+                  f" Correct answer was '{right_answer}'")
+            print(f"Let's try again, {user_name}!")
+            break
+
+        if correct_answers == 3:
+            print(f'Congratulations, {user_name}!')
+
+
+if __name__ == '__main__':
+    main()
